0559911dfb8900cea71abf629b9fe03f52d8e2e1
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-07-26 14:11:32 +0300

    fixed coolAdd

--- src/helpers/coolAdd/coolAdd.js
+++ src/helpers/coolAdd/coolAdd.js
@@ -1,21 +1,9 @@
 'use strict';
 
 const coolAdd = (...args) => {
-  const sumOfArgs = (arrayOfArgs) => {
-    return arrayOfArgs.reduce((acc, currentValue) => acc + currentValue);
-  };
-
-  let currentSum = sumOfArgs(args);
-
-  const nextFunc = (...nextArgs) => {
-    const nextArgsSum = sumOfArgs(nextArgs);
-    currentSum += nextArgsSum;
-    return nextFunc;
-  };
-
-  nextFunc.toString = () => currentSum;
-  nextFunc.valueOf = () => currentSum;
-  return nextFunc;
+  return args.length === 2
+    ? args.reduce((a, b) => a + b)
+    : b => args[0] + b;
 };
 
 module.exports = coolAdd;
--- src/helpers/coolAdd/coolAdd.test.js
+++ src/helpers/coolAdd/coolAdd.test.js
@@ -2,12 +2,6 @@
 
 const coolAdd = require('./coolAdd');
 
-//here '+' because the function returns a function
-test('for coolAdd(1)', () => {
-  expect(+coolAdd(1))
-    .toBe(1);
-});
-
 test('for coolAdd(1, 2)', () => {
   expect(+coolAdd(1, 2))
     .toBe(3);
@@ -16,24 +10,4 @@ test('for coolAdd(1, 2)', () => {
 test('for coolAdd(1)(2)', () => {
   expect(+coolAdd(1)(2))
     .toBe(3);
-});
-
-test('for coolAdd(1, 2, 3)', () => {
-  expect(+coolAdd(1, 2, 3))
-    .toBe(6);
-});
-
-test('for coolAdd(1)(2)(3)', () => {
-  expect(+coolAdd(1)(2)(3))
-    .toBe(6);
-});
-
-test('for coolAdd(1, 2)(3)', () => {
-  expect(+coolAdd(1, 2)(3))
-    .toBe(6);
-});
-
-test('for coolAdd(1, 2)(3, 4)', () => {
-  expect(+coolAdd(1, 2)(3, 4))
-    .toBe(10);
 });
